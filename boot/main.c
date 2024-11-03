ba1ea37b7053318aee50b94b6e0533c0f70309f7
inseok <inseok@rootech.com>
2024-11-04 08:56:27 +0900

    7장 Timer 동작 완료

--- boot/main.c
+++ boot/main.c
@@ -3,11 +3,14 @@
 
 #include "HalInterrupt.h"
 #include "HalUart.h"
+#include "HalTimer.h"
 
 #include "stdio.h"
+#include "stdlib.h"
 
 static void Hw_init(void);
 static void Printf_test(void);
+static void Timer_test(void);
 
 void main(void){
 	Hw_init();
@@ -21,7 +24,7 @@ void main(void){
 	putstr("Hello World!\n");
 
 	Printf_test();
-
+	Timer_test();
 	while(true);
 	
 }
@@ -29,12 +32,16 @@ void main(void){
 static void Hw_init(void){
 	Hal_interrupt_init();
 	Hal_uart_init();
+	Hal_timer_init();
+
 }
 
 static void Printf_test(void){
 	char* str = "printf pointer test";
 	char* nullptr = 0;
 	uint32_t i = 5;
+    uint32_t* sysctrl0 = (uint32_t*)0x10001000;
+
 
 	debug_printf("%s\n", "Hello printf");
 	debug_printf("output string pointer : %s\n", str);
@@ -42,5 +49,14 @@ static void Printf_test(void){
 	debug_printf("%u = 5\n", i);
 	debug_printf("dec = %u hex=%x\n", 0xff, 0xff);
 	debug_printf("print zero %u\n", 0);
+    debug_printf("SYSCTRL0 %x\n", *sysctrl0);
 
+}
+
+
+static void Timer_test(void){
+	while(true){
+		debug_printf("Current count : %u\n", Hal_timer_get_1ms_counter());
+		delay(1000);
+	}
 }
